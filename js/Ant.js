55a25fb596a704683ba4d676fed431482bd4d9ac
haridulal1000 <84311145+haridulal1000@users.noreply.github.com>
2021-12-28 02:54:36 +0545

    first

---
+++ js/Ant.js
@@ -0,0 +1,74 @@
+function Ball(info) {
+  this.radius = info.radius;
+  this.x = info.x;
+  this.y = info.y;
+  this.frameCount=0;
+  this.xVel = info.xVel;
+  this.yVel = info.yVel;
+  this.img=new Image();
+  this.img.src='../images/ant-walk.png';
+  this.show = function () {
+    const no=8;
+    const totalNo=62;
+    const index=this.frameCount%totalNo;
+    const yIndex=Math.floor(index/no);
+    const xIndex=index%no;
+    const w=50;
+    const h=100;
+    context.save();
+    let ratio=this.yVel/this.xVel;
+    context.translate(this.x,this.y);
+    if(this.xVel<0 && this.yVel<0){
+    context.rotate(Math.atan(ratio)-Math.PI/2);
+    }
+    else if(this.xVel>0 && this.yVel<0){
+      context.rotate(Math.atan(ratio)+Math.PI/2);
+    }
+    else if(this.xVel<0 && this.yVel>0){
+      context.rotate(Math.atan(ratio)-Math.PI/2);
+    }
+    else{
+      context.rotate(Math.atan(ratio)+Math.PI/2);
+    }
+    context.drawImage(this.img,xIndex*w,yIndex*h,w,h,-this.radius,-this.radius,this.radius*2,this.radius*2);
+    context.restore();
+    
+
+  
+    this.frameCount++;
+  };
+  this.update = function () {
+    if (this.x + this.radius >= width || this.x - this.radius < 0) {
+      this.xVel *= -1;
+    }
+    if (this.y + this.radius >= height || this.y - this.radius < 0) {
+      this.yVel *= -1;
+    }
+    this.x += this.xVel;
+    this.y += this.yVel;
+  };
+
+  this.collides = function (ball) {
+    let dist = distance(this.x, this.y, ball.x, ball.y);
+    if (dist <= this.radius + ball.radius && this !== ball) {
+      let distX = this.x - ball.x;
+      let distY = this.y - ball.y;
+      let penDepth = this.radius + ball.radius - dist;
+      let unitDistX = distX / Math.sqrt(distX ** 2 + distY ** 2);
+      let unitDistY = distY / Math.sqrt(distX ** 2 + distY ** 2);
+      let penResX = (unitDistX * penDepth) / 2;
+      let penResY = (unitDistY * penDepth) / 2;
+      this.x += penResX;
+      this.y += penResY;
+      ball.x -= penResX;
+      ball.y -= penResY;
+
+      let tempX = ball.xVel;
+      let tempY = ball.yVel;
+      ball.xVel = this.xVel;
+      ball.yVel = this.yVel;
+      this.xVel = tempX;
+      this.yVel = tempY;
+    }
+  };
+}
---
+++ js/index.js
@@ -0,0 +1,56 @@
+let canvas = document.getElementById("viewPort");
+let button = document.getElementById("reset");
+let height = "650";
+let width = "1400";
+const  number=100;
+let ballInfo;
+let balls = [];
+let animation;
+canvas.setAttribute("height", height);
+canvas.setAttribute("width", width);
+canvas.style.border = "1px solid black";
+let context = canvas.getContext("2d");
+let loop = function () {
+  animation = window.requestAnimationFrame(draw);
+};
+function setup() {
+  context.fillStyle = "white";
+  context.fillRect(0, 0, width, height);
+  for (let i = 0; i < number; i++) {
+    balls.push(new Ball(generateElement()));
+  }
+  loop();
+}
+function draw() {
+  context.fillStyle = "white";
+  context.fillRect(0, 0, width, height);
+
+  for (let i = 0; i < balls.length; i++) {
+    for (let j = i + 1; j < balls.length; j++) {
+      balls[i].collides(balls[j]);
+    }
+  }
+
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].update();
+    balls[i].show();
+  }
+  loop();
+}
+
+setup();
+button.addEventListener("click", function () {
+  window.cancelAnimationFrame(animation);
+  balls = [];
+  setup();
+});
+canvas.addEventListener('click',function(e){
+  const audio = new Audio('./sounds/sound.mp3');
+    audio.play();
+  for(let i=balls.length-1;i>=0;i--){
+    let dist=distance(balls[i].x,balls[i].y,e.clientX,e.clientY);
+    if(dist<balls[i].radius){
+      balls.splice(i,1);
+    }
+  }
+});
